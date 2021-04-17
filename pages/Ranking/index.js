8b89c488b02edd112d74fffebf9bd68c5cb10319
Daniel Fernando Lourusso <dflourusso@gmail.com>
2021-04-17 11:56:25 -0300

    feat: add reset ranking button

--- pages/Ranking/index.js
+++ pages/Ranking/index.js
@@ -1,20 +1,43 @@
 import React, { useEffect, useState } from 'react'
 import { StatusBar } from 'expo-status-bar'
-import { FlatList, StyleSheet, Text, TouchableOpacity } from 'react-native'
+import { Alert, FlatList, StyleSheet, Text, TouchableOpacity } from 'react-native'
 import { Container } from './styles'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import RankingCard from '../../components/RankingCard'
 import RankingEmpty from '../../components/RankingEmpty'
+import Link from '../../components/Link'
 
 
 export default function Ranking({ navigation }) {
   const [ranking, setRanking] = useState([])
 
-  useEffect(() => {
+  const loadRanking = () => {
     AsyncStorage.getItem('ranking')
       .then(ranking => {
         if (ranking) setRanking(JSON.parse(ranking))
       })
+  }
+
+  const resetRanking = () => {
+    Alert.alert('Redefinir o ranking?', 'Todo o histórico será perdido', [
+      { text: 'Agora não' },
+      {
+        text: 'Redefinir',
+        style: 'destructive',
+        onPress: () => {
+          AsyncStorage.removeItem('ranking').then(() => {
+            setRanking([])
+          })
+            .catch(() => {
+              Alert.alert('Ocorreu um problema ao redefinir o ranking')
+            })
+        }
+      },
+    ])
+  }
+
+  useEffect(() => {
+    loadRanking()
   }, [])
 
   const renderItem = ({ item, index }) => (
@@ -31,6 +54,7 @@ export default function Ranking({ navigation }) {
         keyExtractor={item => item.playerName}
         renderItem={renderItem}
         ListEmptyComponent={<RankingEmpty />}
+        ListFooterComponent={<Link onPress={resetRanking}>Redefinir ranking</Link>}
         showsVerticalScrollIndicator={false}
       />
     </Container>
