6cd9839c647f7c229250c8362c42e990f4392e00
Daniel Fernando Lourusso <dflourusso@gmail.com>
2021-04-17 13:25:10 -0300

    refactor: remode debug try catch

--- redux/sagas/ranking.js
+++ redux/sagas/ranking.js
@@ -3,26 +3,20 @@ import { takeEvery, put, select, call } from 'redux-saga/effects'
 import { updateListItem } from '../helpers';
 
 function* onGameEnd(action) {
-  try {
-    const { playerName, rounds } = action.payload
+  const { playerName, rounds } = action.payload
 
-    let ranking = JSON.parse(yield call(AsyncStorage.getItem, 'ranking')) || []
-    const lastGame = ranking.find(item => item.playerName === playerName)
+  let ranking = JSON.parse(yield call(AsyncStorage.getItem, 'ranking')) || []
+  const lastGame = ranking.find(item => item.playerName === playerName)
 
-    if (lastGame) {
-      const bestMatch = lastGame ? Math.min(Number(lastGame.bestMatch), rounds) : rounds
-      ranking = updateListItem(ranking, { playerName, bestMatch }, 'playerName')
-    } else {
-      ranking = [...ranking, { playerName, bestMatch: rounds }]
-    }
-
-    const value = JSON.stringify(ranking.sort((a, b) => (a.bestMatch > b.bestMatch) ? 1 : -1))
-    console.log(value)
-    yield call(AsyncStorage.setItem, 'ranking', value)
-
-  } catch (error) {
-    console.log(error)
+  if (lastGame) {
+    const bestMatch = lastGame ? Math.min(Number(lastGame.bestMatch), rounds) : rounds
+    ranking = updateListItem(ranking, { playerName, bestMatch }, 'playerName')
+  } else {
+    ranking = [...ranking, { playerName, bestMatch: rounds }]
   }
+
+  const value = JSON.stringify(ranking.sort((a, b) => (a.bestMatch > b.bestMatch) ? 1 : -1))
+  yield call(AsyncStorage.setItem, 'ranking', value)
 }
 
 export default function* helloSaga() {
